f8bddf9a249bc84d6abc59dd75c011022001252a
yacai01 <yacai01@mail.yst.com.cn>
2020-08-19 14:29:41 +0800

    remove aop

--- candies.py
+++ candies.py
@@ -1,5 +1,4 @@
 from proto import *
-from config import *
 
 class Candy:
     
@@ -25,20 +24,17 @@ import traceback
 
 class _Exception(Candy):
 
-    def decorator_catch_exception(self, func):
-        def wrapper(*args, **kwargs):
-            try:
-                return func(*args, **kwargs)
-            except Exception:
-                traceback.print_exc()
-        return wrapper
-    
-    def decorator_ignore_exception(self, func):
-        def wrapper(*args, **kwargs):
-            try:
-                return func(*args, **kwargs)
-            except Exception:
-                pass
+    def decorator_catch_exception(self, Type, handler=None):
+        def wrapper(func):
+            def inner_wrapper(*args, **kwargs):
+                try:
+                    return func(*args, **kwargs)
+                except Type as e:
+                    if handler is not None:
+                        handler(e)
+                    else:
+                        traceback.print_exc()
+            return inner_wrapper
         return wrapper
 
 exec(register)
@@ -48,12 +44,12 @@ from concurrent.futures import ThreadPoolExecutor
 class _Concurrent(Candy):
 
     def __init__(self):
-        self.pool = ThreadPoolExecutor(max_workers=threads)
+        self.pool = ThreadPoolExecutor(max_workers=10)
         self.intervals = {}
     
-    def _set_max_workers(self, value):
+    def export_set_max_workers(self, value):
         self.pool._max_workers = value
-    
+
     def task_wrapper(self, func, sleep, *args, **kwargs):
         @catch_exception
         def task():
@@ -98,6 +94,11 @@ class _Concurrent(Candy):
                     return self.interval_task_wrapper(func, name, interval/1000, *args, **kwargs)
                 return inner_wrapper
         return wrapper
+    
+    def decorator_immediate(self, *args, **kwargs):
+        def wrapper(func):
+            return func(*args, **kwargs)
+        return wrapper
 
 exec(register)
 
@@ -112,130 +113,6 @@ class _Timer(Candy):
                 return value
             return inner_wrapper
         return wrapper
-
-class _AOP(Candy):
-
-    def decorator_aop(self, before=None, after=None):
-        def wrapper(func):
-            def inner_wrapper(*args, **kwargs):
-                if callable(before):
-                    before(*args, **kwargs)
-                value = func(*args, **kwargs)
-                if callable(after):
-                    after(*args, **kwargs)
-                return value
-            return inner_wrapper
-        return wrapper
-
-exec(register)
-
-import re
-import requests
-
-from xml.sax.saxutils import unescape
-
-
-class _HTTPData(Candy):
-
-    def __init__(self):
-        self.data = None
-        self._cookies = None
-        self._session = requests.Session()
-
-    def export_result(self):
-        return self.data
-
-    def decorator_embed(self, func):
-        exec(f'self.{func.__name__} = func', globals(), locals())
-
-    def decorator_src(self, url, headers=None, data=None, method='get', mode='t', coding='utf-8', consumes=None, inherit_cookies=False, new_session=False):
-        def wrapper(func):
-            def inner_wrapper(*args, **kwargs):
-                if new_session:
-                    self._session = requests.Session()
-                _url, _coding, _headers, _data = self._replace_all_params(kwargs, url, coding, headers, data)
-                if _headers is None:
-                    _headers = {}
-                _headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36'})
-                if consumes is not None:
-                    headers['Content-Type'] = consumes
-                if method.lower() == 'post':
-                    resp = self._session.post(_url, headers=_headers, data=_data, cookies=self._cookies if inherit_cookies else None)
-                elif method.lower() == 'get':
-                    resp = self._session.get(
-                        _url + (f'?{"&".join([k + "=" + _data[k] for k in _data])}' if _data is not None else ''),
-                        headers=_headers, cookies=self._cookies if inherit_cookies else None)
-                if not inherit_cookies:
-                    self._cookies = resp.cookies
-                self.data = resp.content
-                if mode == 't':
-                    self.data = self.data.decode(_coding)
-                return func(*args, **kwargs)
-            inner_wrapper.__name__ = func.__name__
-            return inner_wrapper
-
-        return wrapper
-
-    def decorator_flow(self, *handlers):
-        def wrapper(func):
-            def inner_wrapper(*args, **kwargs):
-                self.data = self._handle(self.data, *handlers, **kwargs)
-                return func(*args, **kwargs)
-            inner_wrapper.__name__ = func.__name__
-            return inner_wrapper
-
-        return wrapper
-
-    def _handle(self, data, *handlers, **kwargs):
-        for handler in handlers:
-            if type(handler) == list:
-                result = []
-                for single in handler:
-                    result.append(self._handle(data, single))
-                data = result
-            elif type(handler) == dict:
-                result = {}
-                for key in handler:
-                    single = handler[key]
-                    result_key, = self._replace_all_params(kwargs, self._handle(data, key))
-                    result[result_key] = self._handle(
-                        data, single)
-                data = result
-            elif callable(handler):
-                data = handler(data)
-            else:
-                return handler
-        return data
-
-    def _parse_template(self, template, kwargs, globals, locals):
-        for match in re.findall(r'\<.*?\>', template):
-            statement = match[1:-1]
-            globals.update(kwargs)
-            repl = eval(statement, globals, locals)
-            template = template.replace(match, str(repl))
-        return template
-
-    def _replace_param(self, s, **kwargs):
-        if type(s) == str:
-            return self._parse_template(s, kwargs, globals(), locals())
-        elif type(s) == list:
-            result = []
-            for item in s:
-                result.append(self._replace_param(item, **kwargs))
-            return result
-        elif type(s) == dict:
-            result = {}
-            for key in s:
-                result[self._replace_param(key, **kwargs)] = self._replace_param(s[key], **kwargs)
-            return result
-
-    def _replace_all_params(self, to_repl, *args):
-        new_args = []
-        for arg in args:
-            new_args.append(self._replace_param(arg, **to_repl))
-        return new_args
-
-exec(register)
 
 class _IoC(Candy):
 
--- proto.py
+++ proto.py
@@ -1,4 +1,4 @@
-# JavaScript Style Async
+# JavaScript Style
 def set_interval(name:str, interval:float=0):
     pass
 
@@ -8,32 +8,18 @@ def set_timeout(timeout:float):
 def clear_interval(name:str):
     pass
 
+def set_max_workers(value:int):
+    pass
+
+def immediate(*args, **kwargs):
+    pass
+
 # Timer
 def timer(prefix:str, factor:float=1, suffix:str='s'):
     pass
 
-# AOP
-def aop(before=None, after=None):
-    pass
-
 # Exception
-def catch_exception(func):
-    pass
-
-def ignore_exception(func):
-    pass
-
-# HTTP Data
-def embed(func):
-    pass
-
-def src(url:str, headers:dict=None, data:dict=None, method:str='get', mode:str='t', coding:str='utf-8', consumes:str=None, inherit_cookies:bool=False, new_session:bool=False):
-    pass
-
-def flow(*handlers):
-    pass
-
-def result():
+def catch_exception(func, handler=None):
     pass
 
 # Spring Style IoC
@@ -41,4 +27,8 @@ def component(clazz):
     pass
 
 def get_bean(clazz):
+    pass
+
+# pylombok
+def all_args_constructor(init):
     pass
